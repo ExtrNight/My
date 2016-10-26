faa1bcd82489f825bd43afea0bca81ff2b542470
Administrator <2705579599@qq.com>
2016-10-26 19:27:05 +0800

    第二次更新

--- app/src/main/java/com/king/myapplication/MainActivity.java
+++ app/src/main/java/com/king/myapplication/MainActivity.java
@@ -19,5 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i("jjj", "onCreate: ");Log.i("jjj", "onCreate: ");
         Log.i("jjj", "onCreate: ");
         Log.i("222222222222222", "onCreate: ");
+        Log.i("222222222222222", "onCreate: ");
+
     }
 }
